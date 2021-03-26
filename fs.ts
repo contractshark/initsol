401e5acc0c7d0c1b61c2346b1e51cfb27f453120
sam bacha <sam@freighttrust.com>
2021-03-26 09:37:54 -0700

    feat(hh): fs

---
+++ fs.ts
@@ -0,0 +1,8 @@
+/** @file fs **/
+import path from 'path'
+
+export const getAttackerContractName = (testPath:string) => {
+  const fileNameNoExtension = path.parse(testPath).name
+  return `contracts/${fileNameNoExtension}/Attacker.sol:Attacker`
+}
+/** @exports getAttackerContractName */
