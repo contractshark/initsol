a5b41344f8fdf65d5d10dcbd58eacd614eb109ca
sam bacha <sam@freighttrust.com>
2021-03-26 09:42:08 -0700

    feat(hh): accounts, get-code

---
+++ ts/tasks/index.ts
@@ -0,0 +1,10 @@
+/**
+* @file hardhat tasks
+* @summary: 
+*   for hardhat to pick up these tasks, they need to be imported like this
+*   not with 'import * as tasks from ...'
+*/
+
+import './accounts';
+import './get-code';
+/** @exports [accounts, get-code] */
